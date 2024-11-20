e5ffe9b66ff1da27816fbe5c35052b751cc515f3
Adam Dubey <adubey0311@gmail.com>
2024-11-20 16:29:55 -0500

    [AD][112024] Update orders mailer

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,13 +1,14 @@
 class OrderMailer < ApplicationMailer
+  default from: "Ocho Depot <depot@example.com>"
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
   #   en.order_mailer.received.subject
   #
-  def received
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+  def received(order)
+    @order = order
+    
+    mail to: order.email, subject: "Rails Ocho Depot Store Order Confirmation"
   end
 
   # Subject can be set in your I18n file at config/locales/en.yml
@@ -16,8 +17,8 @@ class OrderMailer < ApplicationMailer
   #   en.order_mailer.shipped.subject
   #
   def shipped
-    @greeting = "Hi"
+    @order = order
 
-    mail to: "to@example.org"
+    mail to: order.email, subject: "Rails Ocho Depot Store Order Shipped"
   end
 end
