1612355c894c9ee880a906e8b0df39b7a0388a94
Adam Dubey <adubey0311@gmail.com>
2024-11-21 16:49:36 -0500

    [AD][112124] Update tests for authentication

---
+++ test/controllers/admin_controller_test.rb
@@ -0,0 +1,12 @@
+require "test_helper"
+
+class AdminControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    login_as users(:one)
+  end
+
+  test "should get index" do
+    get admin_url
+    assert_response :success
+  end
+end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -18,10 +18,10 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
   test "should create user" do
     assert_difference("User.count") do
-      post users_url, params: { user: { email_address: @user.email_address, name: @user.name, password: "secret", password_confirmation: "secret" } }
+      post users_url, params: { user: { email_address: "customer@example.org", name: "customer", password: "secret", password_confirmation: "secret" } }
     end
 
-    assert_redirected_to user_url(User.last)
+    assert_redirected_to users_url
   end
 
   test "should show user" do
@@ -36,7 +36,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
   test "should update user" do
     patch user_url(@user), params: { user: { email_address: @user.email_address, name: @user.name, password: "secret", password_confirmation: "secret" } }
-    assert_redirected_to user_url(@user)
+    assert_redirected_to users_url
   end
 
   test "should destroy user" do
